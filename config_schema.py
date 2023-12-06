d923558831053509d1b30c06edc28097686dbb4f
Gustav Hansen <gustav5hansen5@gmail.com>
2023-12-06 10:08:41 +0100

    Working code for local and hpc training. Cloud is currently not supported. Local training on M1 is super slow since mps device is not supported for conv3D element in pytorch.

---
+++ config_schema.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+from hydra.core.config_store import ConfigStore
+from omegaconf import MISSING
+from dataclasses import field
+from typing import Any
+from typing import List
+
+def register_config():
+    ConfigStore.instance().store(name="config_schema", node=ConfigSchema)
+
+@dataclass
+class DataConfig:
+    data_path: str = MISSING
+    input_img_size: int = MISSING
+    crop_volume_size: int = MISSING
+    prob_foreground_center: float = MISSING # Probability that center of crop is a labeled foreground voxel (ensures the crops often contain a label)
+
+
+@dataclass
+class TrainingConfig:
+    device: str = MISSING
+    train_batch_size: int = MISSING
+    val_batch_size: int = MISSING
+    batches_per_epoch: int = MISSING
+    num_epochs: int = MISSING
+    loss_fn_name: str = MISSING
+
+
+@dataclass
+class OptimizerConfig:
+    learning_rate: float = MISSING
+
+
+@dataclass
+class ModelConfig:
+    model_type: str = MISSING
+
+
+@dataclass
+class ConfigSchema:
+    run_name: str = MISSING
+    data: DataConfig = DataConfig()
+    training: TrainingConfig = TrainingConfig()
+    optimizer: OptimizerConfig = OptimizerConfig()
+    model: ModelConfig = ModelConfig()
+    hydra: Any = field(default_factory=lambda: {"job": {"chdir": False}, "run": {"dir": "."}})
+    defaults: List[Any] = field(
+        default_factory=lambda: [
+            "_self_",
+            {"override hydra/hydra_logging": "none"},
+            {"override hydra/job_logging": "none"},
+        ]
+    )
---
+++ utils.py
@@ -0,0 +1,102 @@
+import numpy as np
+from skimage.measure import label as skimage_label, regionprops
+from typing import Tuple, List, Dict
+
+from monai.losses import MaskedDiceLoss
+from monai.transforms import (
+    Compose,
+    CopyItemsd,
+    EnsureChannelFirstd,
+    FgBgToIndicesd,
+    LabelToMaskd,
+    RandCropByPosNegLabeld,
+    RandRotate90d,
+    RandAxisFlipd,
+)
+
+def extract_label_patches(
+        image: np.ndarray,
+        label: np.ndarray,
+        patch_size: Tuple[int, int, int],
+    ) -> List[Dict[str, np.ndarray]]:
+    """
+    Extract patches from image where label is non-zero.
+
+    For each connected component in label, extract the bounding box.
+    Split the bounding box into overlapping patches of size patch_size.
+    Extract the patches from image and label.
+    Return the patches as a list of { 'image': ..., 'label': ... } dicts.
+
+    Args:
+        image (np.ndarray): Image to extract patches from.
+        label (np.ndarray): Label to extract patches from.
+        patch_size (tuple): Size of the patches to extract.
+
+    Returns:
+        list: List of patches as { 'image': ..., 'label': ..., 'mask': ... } dicts.
+    """
+    props = regionprops(skimage_label(label > 0))  # Extract connected components of labeled voxels
+    patches = []
+    for pp in props:
+        # Extract bounding box for connected component
+        cc_min = pp.bbox[:3]
+        cc_max = pp.bbox[3:]
+
+        # Extract patches covering the bounding box
+        for z in range(cc_min[0] - patch_size[0] // 2, cc_max[0] + patch_size[0] // 2, patch_size[0] // 2):
+            for y in range(cc_min[1] - patch_size[1] // 2, cc_max[1] + patch_size[1] // 2, patch_size[1] // 2):
+                for x in range(cc_min[2] - patch_size[2] // 2, cc_max[2] + patch_size[2] // 2, patch_size[2] // 2):
+                    # Ensure patch is within image bounds
+                    z_begin = max(z, 0)
+                    y_begin = max(y, 0)
+                    x_begin = max(x, 0)
+                    z_end = min(z + patch_size[0], image.shape[0])
+                    y_end = min(y + patch_size[1], image.shape[1])
+                    x_end = min(x + patch_size[2], image.shape[2])
+
+                    patch_label = label[z_begin:z_end, y_begin:y_end, x_begin:x_end]
+                    if not patch_label.any():
+                        # Skip empty patches
+                        continue
+                    patch_image = image[z_begin:z_end, y_begin:y_end, x_begin:x_end]
+
+                    if patch_image.shape != patch_size:
+                        # Pad patch if it is smaller than patch_size
+                        pad_size = [(0, 0)] * 3
+                        for d in range(3):
+                            pad_size[d] = (0, patch_size[d] - patch_image.shape[d])
+                        patch_image = np.pad(patch_image, pad_size, 'constant', constant_values=0)
+                        pad_size = [(0, 0)] * 3
+                        for d in range(3):
+                            pad_size[d] = (0, patch_size[d] - patch_label.shape[d])
+                        patch_label = np.pad(patch_label, pad_size, 'constant', constant_values=0)
+
+                    patches.append({ 'image': patch_image, 'label': patch_label, 'mask': patch_label > 0 })
+
+    return patches
+
+def get_transforms(patch_size, prob_foreground_center):
+    train_transforms = Compose([
+        EnsureChannelFirstd(keys=['image', 'label'], channel_dim='no_channel'),
+        CopyItemsd(keys=['label'], times=1, names=['mask']),                                                  # Copy label to new image mask
+        LabelToMaskd(keys=['mask'], select_labels=[1, 2], merge_channels=True),                               # Convert mask to binary mask showing where labels are
+        FgBgToIndicesd(keys=['mask'], fg_postfix='_fg_indices', bg_postfix='_bg_indices'),                    # Precompute indices of labelled voxels
+        RandCropByPosNegLabeld(keys=['image', 'label', 'mask'], label_key='label', spatial_size=patch_size,   # Extract random crop
+                                pos=prob_foreground_center, neg=(1.0 - prob_foreground_center),
+                                num_samples=1, fg_indices_key='mask_fg_indices', bg_indices_key='mask_bg_indices'),
+        RandRotate90d(keys=['image', 'label', 'mask'], prob=0.5, spatial_axes=(0, 1)),                        # Randomly rotate
+        RandRotate90d(keys=['image', 'label', 'mask'], prob=0.5, spatial_axes=(1, 2)),                        # Randomly rotate
+        RandRotate90d(keys=['image', 'label', 'mask'], prob=0.5, spatial_axes=(0, 2)),                        # Randomly rotate
+        RandAxisFlipd(keys=['image', 'label', 'mask'], prob=0.1),                                             # Randomly flip
+    ])
+    val_transforms = Compose([
+        EnsureChannelFirstd(keys=['image', 'label', 'mask'], channel_dim='no_channel'),
+    ])
+
+    return train_transforms, val_transforms
+
+def get_loss_fn(loss_fn_name):
+    loss_fn = {
+        "maskedDiceLoss": MaskedDiceLoss(include_background=True)
+    }
+    return loss_fn[loss_fn_name]
